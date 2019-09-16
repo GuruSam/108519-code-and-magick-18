46267f04a9a02cee14b5437022df0d7769c4567e
GuruSam <artem7127.com@inbox.ru>
2019-09-16 11:11:17 +0500

    Учебный проект «Холст»

--- js/stat.js
+++ js/stat.js
@@ -8,6 +8,8 @@ var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var BAR_GAP = 50;
 var TEXT_HEIGHT = 42;
+var TEXT_COLOR = '#000';
+var PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -54,8 +56,8 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
   ctx.font = '16px PT Mono';
-  renderText(ctx, 'Ура вы победили!', getX(20), getY(30), '#000');
-  renderText(ctx, 'Список результатов: ', getX(20), getY(10) + TEXT_HEIGHT, '#000');
+  renderText(ctx, 'Ура вы победили!', getX(20), getY(30), TEXT_COLOR);
+  renderText(ctx, 'Список результатов: ', getX(20), getY(52), TEXT_COLOR);
 
   var maxTime = getMaxElement(times);
 
@@ -63,11 +65,11 @@ window.renderStatistics = function (ctx, names, times) {
     var newBarHeight = (BAR_HEIGHT * times[i]) / maxTime;
     var barHeightGap = BAR_HEIGHT - newBarHeight + TEXT_HEIGHT;
     var barWidthGap = getX(40) + (BAR_WIDTH + BAR_GAP) * i;
-    var barColor = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : getBarColor(250);
+    var barColor = names[i] === 'Вы' ? PLAYER_COLOR : getBarColor(250);
     var time = Math.ceil(times[i]);
 
-    renderText(ctx, time, barWidthGap, getY(30) + barHeightGap, '#000');
+    renderText(ctx, time, barWidthGap, getY(30) + barHeightGap, TEXT_COLOR);
     renderBar(ctx, barWidthGap, getY(40) + barHeightGap, newBarHeight, barColor);
-    renderText(ctx, names[i], barWidthGap, getY(252), '#000');
+    renderText(ctx, names[i], barWidthGap, getY(252), TEXT_COLOR);
   }
 };
