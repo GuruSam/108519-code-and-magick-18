a49429930a50958d9683c4cadde423ac68602fcd
GuruSam <artem7127.com@inbox.ru>
2019-09-19 12:44:54 +0500

    Учебный проект: нас орда

--- js/setup.js
+++ js/setup.js
@@ -1,26 +1,25 @@
 'use strict';
 
 var WIZARD_AMOUNT = 4;
-
-var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var randomArrayItem = function (arr) {
   var i = Math.floor(Math.random() * arr.length);
   return arr[i];
 };
 
-var genWizards = function (total) {
+var genWizards = function (names, surnames, coats, eyes, total) {
   var arr = [];
 
   for (var i = 0; i < total; i++) {
     var wizard = {};
 
     wizard.name = randomArrayItem(names) + ' ' + randomArrayItem(surnames);
-    wizard.coatColor = randomArrayItem(coatColors);
-    wizard.eyesColor = randomArrayItem(eyesColors);
+    wizard.coatColor = randomArrayItem(coats);
+    wizard.eyesColor = randomArrayItem(eyes);
 
     arr.push(wizard);
   }
@@ -54,7 +53,7 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-var wizards = genWizards(WIZARD_AMOUNT);
+var wizards = genWizards(NAMES, SURNAMES, COAT_COLORS, EYES_COLORS, WIZARD_AMOUNT);
 
 setupDialog.classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
